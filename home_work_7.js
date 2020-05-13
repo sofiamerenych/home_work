de29b00c24ad2d890af0bf9e84a6b9b2acd65281
Sofiya <64265066+sofiamerenych@users.noreply.github.com>
2020-05-13 11:35:03 +0300

    Finished homework 7

--- home_work_7.js
+++ home_work_7.js
@@ -1,13 +1,54 @@
 // 1. Створіть функцію за допомогою "function expression".
 
+
+let name = 'Simon'
+greeting ()
+
 // 2. Створіть функцію за допомогою "new Function".
+
+let greetingNew = new Function  ('name1', 'console.log ("Hello, " + name1)')
+greetingNew ('Petro')
 
 // 3. Створіть функцію за допомогою "arrow function".
 
+let greetingArrow = (name2) => console.log ('Hello, ' + name2)
+let name2 = 'Mary'
+greetingArrow (name2)
+
 // 4. Створіть функцію яка приймає два аргументи, перший це обєкт другий це колбек.
 
+function hi (obj, cb) {
+  alert ('Hello')
+}
+
+function bye (){
+   alert ('Bye-bye') 
+}
+
+hi ({}, bye())
+
 // 5. Створіть рекурсивну функцію.
+let arr = ['green', ['yellow', 'red', ['black', 'white']], 'purple', ['orange', 'blue']]
+
+function showArr (arg1) {
+    for (let i = 0; i < arg1.length; i++) {
+        console.log (arg1[i])
+        if (Array.isArray (arg1[i])) {
+            showArr (arg1[i])
+        } 
+    }
+}
 
 // 6. Створіть самовикликаючусь функцію.
 
+(function (name3) {
+    console.log ('Hello, ' + name3)
+}) ('Oleg')
+
 // 7. Створіть функцію і виведіть в консоль всі аргументи які були їй передані.
+
+function func2 (a, b, c, d, e){
+    console.log (arguments, typeof (arguments))
+}
+
+func2 (func2, 'what', false, null, 3)
