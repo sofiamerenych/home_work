91835808bf5cf00d1e020553527d8ae953bb3233
Sofiya <64265066+sofiamerenych@users.noreply.github.com>
2020-05-20 21:31:36 +0300

    Update home_work_10.js

--- home_work_10.js
+++ home_work_10.js
@@ -2,6 +2,27 @@
 
 //2 Просто функції
 
+let createPerson = (name, surname) => {
+    let Person = {}
+    Person.name = name
+    Person.surname = surname
+    return `${name} ${surname}`
+}
+
 //3 Функції конструктору
 
+function Person (name, surname){
+    this.name = name
+    this.surname = surname
+    return `${name} ${surname}`
+}
+
 //4 Класу
+
+let Person = class {
+    constructor (name, surname){
+        this.name = name
+        this.surname = surname
+        return `${name} ${surname}`
+    }
+}
